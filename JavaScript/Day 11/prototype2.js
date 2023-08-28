97cc2dc8a1ec30602871d203a281b171468007c2
Sharmishtha Shukla <beinggarffield@gmail.com>
2023-08-28 11:40:03 +0530

    Implemented structure of connected objects in javascript

---
+++ JavaScript/Day 11/prototype2.js
@@ -0,0 +1,28 @@
+function Origin(){
+    this.country="India";
+}
+Employee.prototype= Object.create(Origin.prototype);
+Employee.prototype.constructor = Employee;
+
+function Employee(dep,mob){
+    this.department = dep;
+    this.mobile = mob;
+}
+
+function Manager(name){
+    Employee.call(this);//this and call are used together to make the employee object part of manager object
+    this.name = name;
+}
+Manager.prototype= Object.create(Employee.prototype);
+Manager.prototype.constructor = Manager;
+let Shanu = new Manager("Shanu");
+console.log(Shanu);
+console.log(Shanu.__proto__);
+console.log(Shanu.__proto__.__proto__);//{} because it is not inheriting any other class
+
+
+/* OUTPUT
+Manager { department: undefined, mobile: undefined, name: 'Shanu' }
+Employee { constructor: [Function: Manager] }
+Origin { constructor: [Function: Employee] }
+*/
