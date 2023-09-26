51ce26646af2dc5b89ad5258493d2607e7ac3f2e
Sharmishtha Shukla <beinggarffield@gmail.com>
2023-09-26 18:34:06 +0530

    Implemented router in react for home,contact and about page

--- React/timer/src/App.js
+++ React/timer/src/App.js
@@ -1,14 +1,17 @@
-import Home from "./component/Home";
-import About from "./component/About";
-import Contact from "./component/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact"
+import {Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
      <h1>Hello</h1>
-     <Home />
-     <About />
-     <Contact />
+     <Routes>
+     <Route path="/" element={<Home />}></Route>
+     <Route path="/about" element={<About />}></Route>
+     <Route path="/contact" element={<Contact />}></Route>
+     </Routes>
     </div>
   );
 }
