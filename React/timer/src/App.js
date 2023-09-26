111d9696844f65f5a6ba4b3cb9b863ae8b837a0b
Sharmishtha Shukla <beinggarffield@gmail.com>
2023-09-26 15:56:40 +0530

    Set up React app and created another project named timer

---
+++ React/timer/src/App.js
@@ -0,0 +1,16 @@
+import Home from "./component/Home";
+import About from "./component/About";
+import Contact from "./component/Contact";
+
+function App() {
+  return (
+    <div>
+     <h1>Hello</h1>
+     <Home />
+     <About />
+     <Contact />
+    </div>
+  );
+}
+
+export default App;
