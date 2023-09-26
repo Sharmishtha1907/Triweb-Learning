3859ad71714a5d028175b2f730780189be42009a
Sharmishtha Shukla <beinggarffield@gmail.com>
2023-09-26 14:45:48 +0530

    Set up React app and created first react application

---
+++ React/app/src/App.js
@@ -0,0 +1,11 @@
+import FirstComponent from "./component/FirstComponent";
+
+function App() {
+  return (
+    <div>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/app/src/component/FirstComponent.jsx
@@ -0,0 +1,8 @@
+function FirstComponent(){
+    return <div>
+        <h1>My react application</h1>
+        <p> Its my first react application</p>
+      </div>
+}
+
+export default FirstComponent;
